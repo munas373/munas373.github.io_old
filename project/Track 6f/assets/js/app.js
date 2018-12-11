36d97d428d0f5da5407df1caf4a240bf45b5e1ec
KARISSA <munas373@newschool.edu>
2018-12-10 22:13:46 -0500

    Added Track 6A - 6F

--- project/Track 6f/assets/js/app.js
+++ project/Track 6f/assets/js/app.js
@@ -5,11 +5,5 @@ $('.nav-item').click(function() {
     $('.content').addClass(button_name);
     $('.nav-item').removeClass('active');
     $(this).addClass('active');
-    $('.about-text').addClass('hide');
-  });
-  
-  $('.close').click(function() {
-    $('.slay').addClass('hide');
-    $('.nav-item').removeClass('active');
-    $('audio')[0].pause();
+    $('.just').addClass('hide');
   });
